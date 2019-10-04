ee84a7c50860cea4aa38051953877af05102e634
IMS\Dheveshp <dheveshp@gmail.com>
2019-10-04 14:44:11 +0100

    app refactor. split client and server.

--- client/scripts/ajax.js
+++ client/scripts/ajax.js
@@ -1,17 +1,51 @@
+// DISPLAY
+
+$.ajax({
+    url : 'http://localhost:3000/todos'
+}).done(getTodos);
+
+$('.btn-ico').on('click', ()=>{
+    $('.new-item').toggle();
+    $('#new-todo-item').focus();
+    
+});
+
+function getTodos(todos){
+    for (todo of todos){
+        $("#todo-list").append(
+            `
+            <li class="list-group-item">
+            <form id="editItem" class="editItem" action="/todos/${todo._id}" method="POST">
+                <input id="${todo._id} type="text" value="${todo.text}" name="todo[text]" class="form-control mb-4" required>
+            <button class="btn btn-sm btn-success">Update Item</button>
+            </form>
+            <span class="lead">
+                ${todo.text}
+            </span>
+            <div class="d-inline float-right">
+                <button class="btn btn-sm btn-warning edit-button">Edit</button>
+                <form id="removeItem" class="form d-inline" method="POST" action="/todos/${todo._id}">
+                    <button type="submit" class="btn btn-sm btn-danger delete-button">Delete</button>
+                </form>
+            </div>
+            <div class="clearfix"></div>
+            </li>
+            `
+        );
+    }
+}
+
 // NEW ITEM
 $('#newItem').submit(function(e){
     e.preventDefault();
     var todoItem = $(this).serialize();
-    $.post("/todos", todoItem, function(newTodo){
+    $.post("http://localhost:3000/todos", todoItem, function(newTodo){
         $("#todo-list").append(
             `
             <li class="list-group-item">
             <form id="editItem" class="editItem" action="/todos/${newTodo._id}" method="POST">
-            <div class="form-group">
-                <label for="${newTodo._id}">Item Text</label>
-                <input id="${newTodo._id} type="text" value="${newTodo.text}" name="todo[text]" class="form-control">
-            </div>
-            <button class="btn btn-primary">Update Item</button>
+                <input id="${newTodo._id} type="text" value="${newTodo.text}" name="todo[text]" class="form-control mb-4" required>
+            <button class="btn btn-sm btn-success">Update Item</button>
             </form>
             <span class="lead">
                 ${newTodo.text}
@@ -28,21 +62,33 @@ $('#newItem').submit(function(e){
         );
     });
     $(this).find(".form-control").val("");
+    $(this).find('.form-control').focus();
 });
 
 // EDIT FUNCTIONALITY
 
 $("#todo-list").on("click", ".edit-button", function(){
     $(this).parent().siblings("#editItem").toggle();
+    $(this).parent().siblings("#editItem").trigger('reset');
+    $(this).parent().siblings("form").children('input').focus();
+    if ($(this).text() == 'Edit'){
+        $(this).text('Cancel');
+        $(this).removeClass('btn-warning');
+        $(this).addClass('btn-secondary');
+    } else {
+        $(this).text('Edit');
+        $(this).removeClass('btn-secondary');
+        $(this).addClass('btn-warning');
+    }
 });
 
 $('#todo-list').on("submit", ".editItem", function(e){
     e.preventDefault();
     var todoItem = $(this).serialize();
-    var formAction = $(this).attr('action');
+    var formAction = "http://localhost:3000" + $(this).attr('action');
     $originalItem = $(this).parent(".list-group-item");
     $.ajax({
-        url: formAction,
+        url:  formAction,
         data: todoItem,
         type: 'PUT',
         originalItem : $originalItem,
@@ -50,11 +96,8 @@ $('#todo-list').on("submit", ".editItem", function(e){
             this.originalItem.html(
            `
            <form id="editItem" class="editItem" action="/todos/${todo._id}" method="POST">
-                <div class="form-group">
-                    <label for="${todo._id}">Item Text</label>
-                    <input id="${todo._id}" type="text" value="${todo.text}" name="todo[text]" class="form-control">
-                </div>
-                <button class="btn btn-primary">Update Item</button>
+                    <input id="${todo._id}" type="text" value="${todo.text}" name="todo[text]" class="form-control mb-4" required>
+                <button class="btn btn-sm btn-success">Update Item</button>
                 </form>
                 <span class="lead">
                     ${todo.text}
@@ -78,7 +121,7 @@ $("#todo-list").on("submit", "#removeItem", function(e){
     e.preventDefault();
     var confirmResponse = confirm("Are you sure?");
     if (confirmResponse){
-        var actionUrl = $(this).attr("action");
+        var actionUrl = "http://localhost:3000" + $(this).attr('action');
         $itemToDelete = $(this).closest(".list-group-item");
         $.ajax({
             url: actionUrl,
--- server/app.js
+++ server/app.js
@@ -1,25 +1,24 @@
-var express = require("express"),
-app     = express(),
-mongoose = require("mongoose"),
-bodyParser = require("body-parser"),
-expressSanitizer = require("express-sanitizer"),
-methodOverride = require('method-override');
+const express = require("express");
+const app     = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost:27017/todo_app", { useNewUrlParser : true } );
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
-app.set("view engine", "ejs");
-app.use(methodOverride('_method'));
 
-var todoSchema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema({
   text: String,
 });
 
-var Todo = mongoose.model("Todo", todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/", function(req, res){
-  res.redirect("/todos");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
 
 app.get("/todos", function(req, res){
@@ -27,58 +26,32 @@ app.get("/todos", function(req, res){
     if(err){
       console.log(err);
     } else {
-      if (req.xhr){
         res.json(todos);
-      } else{
-        res.render("index", {todos: todos}); 
-      }
-    }
+      } 
   });
 });
 
-app.get("/todos/new", function(req, res){
- res.render("new"); 
-});
 
 app.post("/todos", function(req, res){
  req.body.todo.text = req.sanitize(req.body.todo.text);
  var formData = req.body.todo;
  Todo.create(formData, function(err, newTodo){
     if(err){
-      res.render("new");
+      console.log(err);
     } else {
-      if (req.xhr){
         res.json(newTodo);
-      } else {
-        res.redirect("/todos");
-      }
-    }
+      } 
   });
 });
 
-app.get("/todos/:id/edit", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-     res.redirect("/")
-   } else {
-      res.render("edit", {todo: todo});
-   }
- });
-});
 
 app.put("/todos/:id", function(req, res){
  Todo.findByIdAndUpdate(req.params.id, req.body.todo, { new : true }, function(err, todo){
    if(err){
      console.log(err);
    } else {
-     if (req.xhr){
        res.json(todo);
-     } else {
-      res.redirect('/');
-     }  
-      
-   }
+     }   
  });
 });
 
@@ -87,12 +60,8 @@ app.delete("/todos/:id", function(req, res){
    if(err){
      console.log(err);
    } else {
-      if (req.xhr){
         res.json(todo);
-      } else {
-        res.redirect("/todos");
-      }
-   }
+      } 
  }); 
 });
 
